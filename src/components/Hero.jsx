50c39a0373f0fb9bbfef7df95bc52af74052b223
MedBuilds <268667762+MedBuilds@users.noreply.github.com>
2026-09-23 19:30:29 +0000

    feat: add hero section

---
+++ src/components/Hero.jsx
@@ -0,0 +1,12 @@
+function Hero(){
+    return (
+        <main className="flex flex-1 flex-col justify-center items-center px-5">
+            <section className="text-center text-shadow-2xs">
+                <h1 className="text-xl sm:text-2xl lg:text-3xl font-semibold text-white">Discover The World and Find Your Way</h1>
+                <p className="text-sm sm:text-xl lg:text-2xl font-light text-gray-200">Your next adventure starts here.</p>
+            </section>
+        </main>
+    )
+}
+
+export default Hero
