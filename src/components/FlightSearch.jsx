53dcef6e7bb358f2eed8af187fa5ffc242cef098
MedBuilds <268667762+MedBuilds@users.noreply.github.com>
2026-09-24 00:38:21 +0000

    style: update app styling

--- src/components/FlightSearch.jsx
+++ src/components/FlightSearch.jsx
@@ -54,14 +54,14 @@ function FlightSearch(){
                     </select>
                     <button 
                         type="button"
-                        className={`${tripType === "round-trip" ? "bg-blue-50" : "hover:bg-blue-50/50"} hidden sm:block px-2 py-1 rounded-lg cursor-pointer transition duration-200`}
+                        className={`${tripType === "round-trip" ? "bg-blue-50 text-primary" : "hover:bg-blue-50/50 text-gray-700"} hidden sm:block px-2 py-1 rounded-lg cursor-pointer transition duration-200`}
                         onClick={()=>setTripType("round-trip")}
                     >
                         Round Trip
                     </button>
                     <button 
                         type="button"
-                        className={`${tripType === "one-way" ? "bg-blue-50" : "hover:bg-blue-50/50"} hidden sm:block px-2 py-1 rounded-lg cursor-pointer transition duration-200`}
+                        className={`${tripType === "one-way" ? "bg-blue-50 text-primary" : "hover:bg-blue-50/50 text-gray-700"} hidden sm:block px-2 py-1 rounded-lg cursor-pointer transition duration-200`}
                         onClick={()=>setTripType("one-way")}
                     >
                         One Way
@@ -113,7 +113,7 @@ function FlightSearch(){
             <div className={`grid grid-cols-1 sm:grid-cols-2 ${tripType === "round-trip" ? "lg:grid-cols-4" : "lg:grid-cols-3"} gap-2`}>
                 {/* Origin */}
                 <label className="flex flex-col gap-2 cursor-pointer bg-white p-3 rounded-lg">
-                    <div className="flex items-center gap-1">
+                    <div className="flex items-center gap-1 text-gray-600">
                         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-plane-takeoff preview-icon size-4 text-primary/60"><path d="M2 22h20"/><path d="M6.36 17.4 4 17l-2-4 1.1-.55a2 2 0 0 1 1.8 0l.17.1a2 2 0 0 0 1.8 0L8 12 5 6l.9-.45a2 2 0 0 1 2.09.2l4.02 3a2 2 0 0 0 2.1.2l4.19-2.06a2.41 2.41 0 0 1 1.73-.17L21 7a1.4 1.4 0 0 1 .87 1.99l-.38.76c-.23.46-.6.84-1.07 1.08L7.58 17.2a2 2 0 0 1-1.22.18Z"/></svg>
                         Origin
                     </div>
@@ -127,7 +127,7 @@ function FlightSearch(){
 
                 {/* Destination */}
                 <label className="flex flex-col gap-2 cursor-pointer bg-white p-3 rounded-lg">
-                    <div className="flex items-center gap-1">
+                    <div className="flex items-center gap-1 text-gray-600">
                         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" stroke-linejoin="round" className="lucide lucide-plane-landing preview-icon size-4 text-primary/60"><path d="M2 22h20"/><path d="M3.77 10.77 2 9l2-4.5 1.1.55c.55.28.9.84.9 1.45s.35 1.17.9 1.45L8 8.5l3-6 1.05.53a2 2 0 0 1 1.09 1.52l.72 5.4a2 2 0 0 0 1.09 1.52l4.4 2.2c.42.22.78.55 1.01.96l.6 1.03c.49.88-.06 1.98-1.06 2.1l-1.18.15c-.47.06-.95-.02-1.37-.24L4.29 11.15a2 2 0 0 1-.52-.38Z"/></svg>                        
                         Destination
                     </div>
@@ -141,7 +141,7 @@ function FlightSearch(){
 
                 {/* Departure Date */}
                 <label className={`flex flex-col gap-2 cursor-pointer bg-white p-3 rounded-lg ${tripType === "one-way" && "sm:col-span-2 lg:col-span-1" }`}>
-                    <div className="flex items-center gap-1">
+                    <div className="flex items-center gap-1 text-gray-600">
                         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-calendar preview-icon size-4 text-primary/60"><path d="M8 2v3"/><path d="M16 2v3"/><rect x="3" y="3" width="18" height="18" rx="2"/><path d="M3 9h18"/></svg>
                         Departure Date
                     </div>
@@ -155,7 +155,7 @@ function FlightSearch(){
                 {/* Return Date */}
                 { tripType === "round-trip" && (
                     <label className="flex flex-col gap-2 cursor-pointer bg-white p-3 rounded-lg">
-                        <div className="flex items-center gap-1">
+                        <div className="flex items-center gap-1 text-gray-600">
                             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-calendar preview-icon size-4 text-primary/60"><path d="M8 2v3"/><path d="M16 2v3"/><rect x="3" y="3" width="18" height="18" rx="2"/><path d="M3 9h18"/></svg>
                             Return Date
                         </div>
