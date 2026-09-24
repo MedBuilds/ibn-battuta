fcbce73d51795965d1c5f2624116c9605cfea126
MedBuilds <268667762+MedBuilds@users.noreply.github.com>
2026-09-24 00:47:28 +0000

    feat: add footer and copyright

---
+++ src/components/Copyright.jsx
@@ -0,0 +1,7 @@
+function Copyright(){
+    return (
+        <p className="text-sm text-white/50 text-center m-3">© {new Date().getFullYear()} Ibn Battuta. All rights reserved.</p>
+    )
+}
+
+export default Copyright
---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import Copyright from "./Copyright";
+
+function Footer(){
+    return (
+        <footer>
+            <Copyright/>
+        </footer>
+    )
+}
+
+export default Footer;
